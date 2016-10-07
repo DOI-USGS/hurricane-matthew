85bb2b1f7e61329b51495b7fac592ff454a5a069
Laura DeCicco <ldecicco@usgs.gov>
2016-10-07 15:16:49 -0500

    Needed bug fix

--- scripts/process/classify.R
+++ scripts/process/classify.R
@@ -1,11 +1,19 @@
 process.classify <- function(viz){
   library(dplyr)
-  
+  library(lubridate)
   #this is just what was left over from my script that wasn't fetch
   discharge <- readData(viz[['depends']][1])
   stats <- readData(viz[['depends']][2])
   
-  finalJoin <- left_join(discharge,stats)
+  discharge <- mutate(discharge,
+                      month_nu = as.integer(month(dateTime)+1),
+                      day_nu = day(dateTime))%>%
+    rename(Flow = Flow_Inst)
+  
+  finalJoin <- left_join(discharge,stats, by=c("site_no"="site_no",
+                                               "agency_cd"="agency_cd",
+                                               "month_nu"="month_nu",
+                                               "day_nu"="day_nu")) 
   
   #classify current discharge values
   finalJoin$class <- NA
