57bf03d38ad508b07c6329030e1f4ddd3cce6476
Laura DeCicco <ldecicco@usgs.gov>
2016-10-07 13:49:30 -0500

    Starting work towards smarter fetching.

--- scripts/process/classify.R
+++ scripts/process/classify.R
@@ -1,7 +1,9 @@
 process.classify <- function(viz){
   #this is just what was left over from my script that wasn't fetch
-  finalJoin <- readData(viz[['depends']])
-  finalJoin <- finalJoin$finalJoin
+  discharge <- readData(viz[['depends']][1])
+  stats <- readData(viz[['depends']][2])
+  
+  finalJoin <- left_join(discharge,stats)
   
   #classify current discharge values
   finalJoin$class <- NA
@@ -10,11 +12,13 @@ process.classify <- function(viz){
   
   finalJoin$class[finalJoin$Flow > finalJoin$p25_va & 
                     finalJoin$Flow <= finalJoin$p50_va] <- "green4"
+  
   finalJoin$class[finalJoin$Flow > finalJoin$p50_va &
                     finalJoin$Flow <= finalJoin$p75_va] <- "blue"
   
   finalJoin$class[is.na(finalJoin$class) & 
                     finalJoin$Flow > finalJoin$p50_va] <- "cyan"
+  
   finalJoin$class[is.na(finalJoin$class) & 
                     finalJoin$Flow < finalJoin$p50_va] <- "yellow"
   
