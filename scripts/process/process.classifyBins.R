8aae8ea3fcc6fddb92ed5b54ee1773e0b27eeec3
wdwatkins <wwatkins@usgs.gov>
2016-10-07 12:15:48 -0500

    almost there?  Where do breaks come from

--- scripts/process/process.classifyBins.R
+++ scripts/process/process.classifyBins.R
@@ -1,11 +1,11 @@
 process.classifyBins <- function(viz){
   #need to get bins
-  colSteps #vector of actual color palette codes
+  colSteps <- readData(viz[['depends']][1]) #vector of actual color palette codes
+  precipData <- readData(viz[['depends']][2]) #actual data
+  precip_breaks <- #???
   
-  precip_breaks
-  
-  #load precip data
+
   precipData$precipVal <- precipData$precipVal/25.4 #convert mm to inches
-  precipData <- totalPrecip %>% mutate(cols = cut(cumprecip, breaks = precip_breaks, labels = colSteps, right=FALSE)) %>% 
+  precipData <- precipData %>% mutate(cols = cut(precipVal, breaks = precip_breaks, labels = colSteps, right=FALSE)) %>% 
     mutate(cols = as.character(cols))
 }
