eb85f1f380c4da3bb0a70e0022c4517f316d5539
Jordan S Read <jread@usgs.gov>
2016-10-07 13:46:52 -0500

    with data

--- scripts/visualize/visualize.matthew_map.R
+++ scripts/visualize/visualize.matthew_map.R
@@ -34,12 +34,15 @@ visualize.matthew_map <- function(viz){
   # find the paths for the counties, add IDs
   p <- xml_find_all(svg, '//*[local-name()="path"]')
   
-  num.time <- 10
-  num.bins <- 9
+  num.time <- group_by(col.bins, fips) %>% tally %>% .$n %>% unique()
+  if (length(num.time) != 1){
+    stop('all of the counties dont have the same number of timesteps!')
+  }
   
   for (i in 1:length(counties)){
     steps <- paste('prcp', 1:num.time, sep='-')
-    time.classes <- paste(steps, sample(1:num.bins, num.time, replace=TRUE), sep='-', collapse=' ') # these are fake
+    bins <- filter(col.bins, fips == as.character(counties$FIPS[i])) %>% .$cols
+    time.classes <- paste(steps, bins, sep='-', collapse=' ') 
     xml_attr(p[[i]], 'id') <- paste0('FIP-', as.character(counties$FIPS[i]))
     xml_attr(p[[i]], 'class') <- sprintf('county-polygon %s', time.classes)
     xml_attr(p[[i]], 'style') <- NULL
