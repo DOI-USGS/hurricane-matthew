cc5ed0d7af317dca86272c8c77e88f5c1b0afc3d
Jordan S Read <jread@usgs.gov>
2016-10-07 06:34:07 -0500

    add flowlines to plot

--- scripts/visualize/visualize.matthew_map.R
+++ scripts/visualize/visualize.matthew_map.R
@@ -1,11 +1,13 @@
 visualize.matthew_map <- function(viz){
   
-  counties <- readData(viz[['depends']])
+  counties <- readData(viz[['depends']][1])
+  flowlines <- readData(viz[['depends']][2])
   library(svglite)
   
   svglite::svglite(viz[['location']])
   par(mai=c(0,0,0,0), omi=c(0,0,0,0))
   sp::plot(counties)
+  sp::plot(flowlines, add=TRUE)
   dev.off()
   
   library(xml2)
@@ -25,9 +27,16 @@ visualize.matthew_map <- function(viz){
   
   for (i in 1:length(counties)){
     xml_attr(p[[i]], 'id') <- paste0('FIP-', as.character(counties$FIPS[i]))
+    xml_attr(p[[i]], 'class') <- 'county-polygon'
     xml_attr(p[[i]], 'style') <- NULL
     xml_attr(p[[i]], 'clip-path') <- NULL
   }
   
+  g.rivers <- xml_add_child(svg, 'g', id='rivers','class'='river-polyline')
+  pl <- xml_find_all(svg, '//*[local-name()="polyline"]')
+  for (i in 1:length(pl)){
+    xml_add_child(g.rivers, 'polyline', points = xml_attr(pl[i], 'points'))
+  }
+  xml_remove(pl)
   write_xml(svg, viz[['location']])
 }
