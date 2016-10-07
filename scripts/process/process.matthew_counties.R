d6a18137b45335381c29199463ad78ffa051a6e1
Jordan S Read <jread@usgs.gov>
2016-10-06 20:57:32 -0500

    I think I am starting to get it...

--- scripts/process/process.matthew_counties.R
+++ scripts/process/process.matthew_counties.R
@@ -1,11 +1,9 @@
 process.matthew_counties <- function(viz){
-  library(rgdal)
   library(rgeos)
   states <- c("FL","GA","SC","NC")
   
   epsg_code <- '+init=epsg:3086' 
-  shp.path <- readData(viz[['depends']])
-  counties = readOGR(shp.path, layer='US_Counties')
+  counties <- readData(viz[['depends']])
   counties = spTransform(countiesCRS(epsg_code))
   
   counties <- counties[counties$STATE %in% states, ]
